946af0522d4e89b4118ac997d53af5c57ed83f09
AndyShar <andyshar1216@msn.com>
2018-07-28 12:20:27 +0800

    add variable type.

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -140,8 +140,8 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
   //set the process covariance matrix Q
   ekf_.Q_ = MatrixXd(4, 4);
 
-  noise_ax = 9;
-  noise_ay = 9;
+  int noise_ax = 9;
+  int noise_ay = 9;
   ekf_.Q_ << dt_4 / 4 * noise_ax, 0, dt_3 / 2 * noise_ax, 0,
              0, dt_4 / 4 * noise_ay, 0, dt_3 / 2 * noise_ay,
              dt_3 / 2 * noise_ax, 0, dt_2 * noise_ax, 0,
